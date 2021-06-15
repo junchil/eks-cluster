04e0138a740d31cc342ac55312297da8b759e461
Steve Liu <ljc1993917@gmail.com>
2021-06-15 20:16:04 +1000

    update

--- golang-app/build.sh
+++ golang-app/build.sh
@@ -1,17 +1,33 @@
 #!/bin/sh
+# NAME is used for the generated executable and as part of the docker image name
+NAME=golang-app
+APPNAME=golang-app
+# PREFIX is prepended to the docker image name before NAME
+PREFIX=eks-cluster/
+# PROJECT_PKG is the Go import path of the top level of the project
+PROJECT_PKG=github.com/junchil/eks-cluster
+# PROJECT_PATH is the GOPATH source path of the top level of the project
+PROJECT_PATH=$(GOPATH)/src/$(PROJECT_PKG)
+# AWS ECR REPO
+ECR_REPO_NAME=$(PREFIX)$(APPNAME)
+# AWS Region for the ECR
+AWS_REGION=ap-southeast-2
+# Commit hash for the service
+COMMIT_HASH=$(git log --pretty=format:%h -n 1 -- ../golang-app)
 
-if [ -d "dist" ]; then
-    rm -rf dist
+RESULT=$(aws ecr describe-images --repository-name $(ECR_REPO_NAME) --region $(AWS_REGION) --image-ids imageTag=$(COMMIT_HASH) | jq '.imageDetails[0].imageTags')
+if [ "$RESULT" = "null" ] || [ -z "$RESULT" ]; then 
+    exit 1;
 fi
 
-mkdir -p dist
+if ! aws --region $(AWS_REGION) ecr describe-repositories --repository-names $(ECR_REPO_NAME); then 
+	aws --region $(AWS_REGION) ecr create-repository --repository-name $(ECR_REPO_NAME);
+fi
 
-dep ensure
-
-go build -o TechTestApp .
-
-cp TechTestApp dist/
-cp -r assets dist/
-cp conf.toml dist/
-
-rm TechTestApp
+aws ecr get-login --no-include-email --region $(AWS_REGION)
+AWS_ECR_REPO_URI=$(aws --region=$(AWS_REGION) ecr describe-repositories --repository-names "$(ECR_REPO_NAME)" | jq -r '.repositories[0].repositoryUri')
+echo "Using AWS ECR uri $(AWS_ECR_REPO_URI)"
+docker build -t $(ECR_REPO_NAME):$(COMMIT_HASH) .
+docker tag $(ECR_REPO_NAME):$(COMMIT_HASH) $(ECR_REPO_NAME):latest
+docker tag $(ECR_REPO_NAME):$(COMMIT_HASH) $(AWS_ECR_REPO_URI):$(COMMIT_HASH)
+docker push $(AWS_ECR_REPO_URI):$(COMMIT_HASH)
