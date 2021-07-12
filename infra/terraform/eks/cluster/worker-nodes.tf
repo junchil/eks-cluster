821890383b42fd24f8a0b8016b9aea2997f41aae
Steve Liu <ljc1993917@gmail.com>
2021-07-12 14:14:27 +1000

    Update worker-nodes.tf

--- infra/terraform/eks/cluster/worker-nodes.tf
+++ infra/terraform/eks/cluster/worker-nodes.tf
@@ -286,7 +286,7 @@ resource "aws_security_group" "worker_node_sg" {
 
   egress {
     from_port   = 0
-    to_port     = 0
+    to_port     = 65535
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
