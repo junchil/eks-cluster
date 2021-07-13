2bee946e196d1b0212a595920dbfa6aa8ac2c8b1
Steve Liu <ljc1993917@gmail.com>
2021-07-13 10:54:30 +1000

    awsdeploy

--- infra/terraform/eks/cluster/sg.tf
+++ infra/terraform/eks/cluster/sg.tf
@@ -48,7 +48,7 @@ resource "aws_security_group" "worker_node_sg" {
 
   egress {
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
