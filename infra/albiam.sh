e437801ae66dd54586713d3e91a21d01bcbcb217
Steve Liu <ljc1993917@gmail.com>
2021-08-27 10:20:31 +1000

    awsdeploy

--- infra/albiam.sh
+++ infra/albiam.sh
@@ -1,7 +1,7 @@
 AWS_REGION=ap-southeast-2
 CLUSTER_NAME=anycluster
 AWS_ACCOUNT_ID=675617749633
-eksctl utils associate-iam-oidc-provider --region $AWS_REGION --cluster $CLUSTER_NAME --approve
-curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json
-aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam-policy.json
+#eksctl utils associate-iam-oidc-provider --region $AWS_REGION --cluster $CLUSTER_NAME --approve
+#curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json
+#aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-document file://iam-policy.json
 eksctl create iamserviceaccount --cluster=$CLUSTER_NAME --name=aws-load-balancer-controller --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy --approve  --override-existing-serviceaccounts
