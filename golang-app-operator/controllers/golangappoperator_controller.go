a1aee1044c79b21af37bbae0a3998d55a2145d03
Steve Liu <ljc1993917@gmail.com>
2022-01-13 16:14:17 +1100

    awsdeploy

--- golang-app-operator/controllers/golangappoperator_controller.go
+++ golang-app-operator/controllers/golangappoperator_controller.go
@@ -44,6 +44,8 @@ type GolangappoperatorReconciler struct {
 //+kubebuilder:rbac:groups=app.stevejcliu.io.stevejcliu.com,resources=golangappoperators,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=app.stevejcliu.io.stevejcliu.com,resources=golangappoperators/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=app.stevejcliu.io.stevejcliu.com,resources=golangappoperators/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
