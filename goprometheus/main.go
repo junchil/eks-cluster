359a5de907dc0c3cd9ba145284ff041f9f6dd967
Steve Liu <ljc1993917@gmail.com>
2022-12-15 11:30:38 +1100

    add vector

---
+++ goprometheus/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+var (
+	appVersion string
+	version    = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "version",
+		Help: "Version information about this binary",
+		ConstLabels: map[string]string{
+			"version": appVersion,
+		},
+	})
+
+	httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "http_requests_total",
+		Help: "Count of all HTTP requests",
+	}, []string{"code", "method"})
+
+	httpRequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "http_request_duration_seconds",
+		Help: "Duration of all HTTP requests",
+	}, []string{"code", "handler", "method"})
+)
+
+func main() {
+	version.Set(1)
+	bind := ""
+	enableH2c := false
+	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	flagset.StringVar(&bind, "bind", ":8080", "The socket to bind to.")
+	flagset.BoolVar(&enableH2c, "h2c", false, "Enable h2c (http/2 over tcp) protocol.")
+	flagset.Parse(os.Args[1:])
+
+	r := prometheus.NewRegistry()
+	r.MustRegister(httpRequestsTotal)
+	r.MustRegister(httpRequestDuration)
+	r.MustRegister(version)
+
+	foundHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello from example application."))
+	})
+	notfoundHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNotFound)
+	})
+	internalErrorHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+	})
+
+	foundChain := promhttp.InstrumentHandlerDuration(
+		httpRequestDuration.MustCurryWith(prometheus.Labels{"handler": "found"}),
+		promhttp.InstrumentHandlerCounter(httpRequestsTotal, foundHandler),
+	)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", foundChain)
+	mux.Handle("/err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, notfoundHandler))
+	mux.Handle("/internal-err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, internalErrorHandler))
+	mux.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+
+	var srv *http.Server
+	if enableH2c {
+		srv = &http.Server{Addr: bind, Handler: h2c.NewHandler(mux, &http2.Server{})}
+	} else {
+		srv = &http.Server{Addr: bind, Handler: mux}
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
