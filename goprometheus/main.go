37a9c770ab4f199befb86a903803b81123460559
Steve Liu <ljc1993917@gmail.com>
2023-11-17 10:05:45 +1100

    awsdeploy

--- goprometheus/main.go
+++ goprometheus/main.go
@@ -21,13 +21,20 @@ func main() {
 	mux := http.NewServeMux()
 	mux.Handle("/metrics", promhttp.Handler())
 	mux.HandleFunc("/", helloServer)
-	mux.HandleFunc("/info", helloServer)
+	mux.HandleFunc("/info", infoServer)
 
 	http.ListenAndServe(":8080", mux)
 }
 
 func helloServer(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!")
+
+	fmt.Println("increasing hello_processed_total counter")
+	opsProcessed.Inc()
+}
+
+func infoServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "info page")
 
 	fmt.Println("increasing hello_processed_total counter")
 	opsProcessed.Inc()
