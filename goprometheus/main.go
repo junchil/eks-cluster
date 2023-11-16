d7d1c259f73f82198a7dd4897a452fa41907fb9a
Steve Liu <ljc1993917@gmail.com>
2023-11-17 09:53:57 +1100

    awsdeploy

--- goprometheus/main.go
+++ goprometheus/main.go
@@ -1,80 +1,34 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
-	"golang.org/x/net/http2"
-	"golang.org/x/net/http2/h2c"
 )
 
 var (
-	appVersion string
-	version    = prometheus.NewGauge(prometheus.GaugeOpts{
-		Name: "version",
-		Help: "Version information about this binary",
-		ConstLabels: map[string]string{
-			"version": appVersion,
-		},
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "hello_processed_total",
+		Help: "The total number of processed calls",
 	})
-
-	httpRequestsTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
-		Name: "steve_http_requests_total",
-		Help: "Count of all HTTP requests",
-	}, []string{"code", "method"})
-
-	httpRequestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
-		Name: "steve_http_request_duration_seconds",
-		Help: "Duration of all HTTP requests",
-	}, []string{"code", "handler", "method"})
 )
 
 func main() {
-	version.Set(1)
-	bind := ""
-	enableH2c := false
-	flagset := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	flagset.StringVar(&bind, "bind", ":8080", "The socket to bind to.")
-	flagset.BoolVar(&enableH2c, "h2c", false, "Enable h2c (http/2 over tcp) protocol.")
-	flagset.Parse(os.Args[1:])
-
-	r := prometheus.NewRegistry()
-	r.MustRegister(httpRequestsTotal)
-	r.MustRegister(httpRequestDuration)
-	r.MustRegister(version)
-
-	foundHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Hello from example application."))
-	})
-	notfoundHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusNotFound)
-	})
-	internalErrorHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusInternalServerError)
-	})
-
-	foundChain := promhttp.InstrumentHandlerDuration(
-		httpRequestDuration.MustCurryWith(prometheus.Labels{"handler": "found"}),
-		promhttp.InstrumentHandlerCounter(httpRequestsTotal, foundHandler),
-	)
-
+	fmt.Println("Starting hello-world server...")
 	mux := http.NewServeMux()
-	mux.Handle("/", foundChain)
-	mux.Handle("/err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, notfoundHandler))
-	mux.Handle("/internal-err", promhttp.InstrumentHandlerCounter(httpRequestsTotal, internalErrorHandler))
-	mux.Handle("/metrics", promhttp.HandlerFor(r, promhttp.HandlerOpts{}))
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/", helloServer)
+	mux.HandleFunc("/info", helloServer)
 
-	var srv *http.Server
-	if enableH2c {
-		srv = &http.Server{Addr: bind, Handler: h2c.NewHandler(mux, &http2.Server{})}
-	} else {
-		srv = &http.Server{Addr: bind, Handler: mux}
-	}
+	http.ListenAndServe(":8080", mux)
+}
 
-	log.Fatal(srv.ListenAndServe())
+func helloServer(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+
+	fmt.Println("increasing hello_processed_total counter")
+	opsProcessed.Inc()
 }
