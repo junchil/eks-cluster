46a9d2d6d823519c7bbd79b1dd4935fe4d87807f
Steve Liu <ljc1993917@gmail.com>
2025-09-24 14:23:40 +1000

    awsdeploy

--- infra/terraform/eks/cluster/s3.tf
+++ infra/terraform/eks/cluster/s3.tf
@@ -33,3 +33,24 @@ resource "aws_s3_bucket" "westfarmers" {
     Name = "${var.cluster_name}-object-store-bucket"
   }
 }
+
+
+resource "aws_s3_bucket" "mimir_admin" {
+  bucket = "mimir-admin"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "mimir_alertmanager" {
+  bucket = "mimir-alertmanager"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "mimir_tsdb" {
+  bucket = "mimir-tsdb"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket" "mimir_ruler" {
+  bucket = "mimir-ruler"
+  force_destroy = true
+}
